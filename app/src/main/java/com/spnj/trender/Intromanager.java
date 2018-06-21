5c99d6bd66879da6be6724a59e2deb780bf83119
Savira Khairani <rani.savira@live.com>
2018-06-21 13:18:15 +0700

    second commit

---
+++ app/src/main/java/com/spnj/trender/Intromanager.java
@@ -0,0 +1,38 @@
+package com.spnj.trender;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by rani.savira on 17/06/18.
+ */
+
+public class Intromanager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "androidhive-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public Intromanager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
