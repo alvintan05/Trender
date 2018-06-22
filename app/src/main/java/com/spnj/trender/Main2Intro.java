c1801148125ad62425aefa16958134974e57f2e7
Alvin <alvintandiardi@gmail.com>
2018-06-23 00:17:25 +0700

    fifth commit

--- app/src/main/java/com/spnj/trender/Main2Intro.java
+++ app/src/main/java/com/spnj/trender/Main2Intro.java
@@ -1,13 +1,39 @@
 package com.spnj.trender;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Intro extends AppCompatActivity {
+
+    private Button signInBtn, signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2_intro);
+
+
+        signInBtn = (Button) findViewById(R.id.signIn);
+        signUpBtn = (Button) findViewById(R.id.signUp);
+
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Intro.this, SignIn.class));
+            }
+        });
+
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Intro.this, SignUp.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/spnj/trender/SignUp.java
+++ app/src/main/java/com/spnj/trender/SignUp.java
@@ -1,13 +1,29 @@
 package com.spnj.trender;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
+
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUpBtn = (Button) findViewById(R.id.btn_createAcc);
+
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUp.this, HomeActivity.class));
+            }
+        });
+
     }
 }
